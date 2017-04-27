a12d55d93e74de517d72ffad8285c92e52e3763a
Stan P <spribytkovsky@yahoo.com>
2017-04-27 16:52:51 -0700

    Repeating data with ngFor implemented and handling Null Values is added

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,15 +3,26 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class='well hoverwell thumbnail'>
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <event-address [location] = 'event.location'></event-address>
-        <button class="btn btn-primary" (click)="handleClick()"> Click! </button>
-    </div>
-    `
+        <div class='well hoverwell thumbnail'>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div>Time: {{event?.time}}</div>
+            <div>Price: \${{event?.price}}</div>
+            <div>
+                <span>Location: {{event?.location?.address}}</span>
+                <span>&nbsp;</span>
+                <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div>
+                Online URL: {{event?.onlineUrl}}
+            </div>
+        </div>
+    `,
+    styles: [`
+      .thumbnail { min-height: 220px; }
+      .pad-left {margin-left: 10px;}
+      .well div { color: #bbb;}
+    `]
 })
 
 export class EventThumbnailComponent {
